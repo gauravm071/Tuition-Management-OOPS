6d8eda940201b8512e04fa6260754a4b28862a46
gauravm071 <gk66048@gmail.com>
2020-11-16 20:35:48 +0530

    all classes deleted

--- src/com/example/tutionmanagement/Grade.java
+++
@@ -1,7 +0,0 @@
-package com.example.tutionmanagement;
-
-public class Grade {
-    boolean hasBoardExamination=true;
-    String board;
-    String shift;
-}
--- src/com/example/tutionmanagement/Subject.java
+++
@@ -1,6 +0,0 @@
-package com.example.tutionmanagement;
-
-public class Subject {
-    String HoursRequiredForOneClass;
-    String classesRequiredForSubjectPerWeek;
-}
