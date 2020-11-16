5e196b4c298621c3b46c851be7db07171bfd83fc
gauravm071 <gk66048@gmail.com>
2020-11-16 21:24:53 +0530

    Subject class created

---
+++ src/com/example/tutionmanagement/Subject.java
@@ -0,0 +1,30 @@
+package com.example.tutionmanagement;
+
+public class Subject {
+    int hoursRequiredForOneClass;
+    int classesRequiredForSubjectPerWeek;
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "hoursRequiredForOneClass=" + hoursRequiredForOneClass +
+                ", classesRequiredForSubjectPerWeek=" + classesRequiredForSubjectPerWeek +
+                '}';
+    }
+
+    public int getHoursRequiredForOneClass() {
+        return hoursRequiredForOneClass;
+    }
+
+    public void setHoursRequiredForOneClass(int hoursRequiredForOneClass) {
+        this.hoursRequiredForOneClass = hoursRequiredForOneClass;
+    }
+
+    public int getClassesRequiredForSubjectPerWeek() {
+        return classesRequiredForSubjectPerWeek;
+    }
+
+    public void setClassesRequiredForSubjectPerWeek(int classesRequiredForSubjectPerWeek) {
+        this.classesRequiredForSubjectPerWeek = classesRequiredForSubjectPerWeek;
+    }
+}
