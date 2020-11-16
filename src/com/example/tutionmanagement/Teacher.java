14717585594dad11ab4bb52bbbdca821bf5deb17
gauravm071 <gk66048@gmail.com>
2020-11-16 21:41:30 +0530

    Teacher class created

--- src/com/example/tutionmanagement/Teacher.java
+++ src/com/example/tutionmanagement/Teacher.java
@@ -10,4 +10,63 @@ public class Teacher {
     ArrayList<Education> listOfEducation= new ArrayList<>();
     ArrayList<Experience> listOfExperience= new ArrayList<>();
 
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "schoolName='" + schoolName + '\'' +
+                ", classGrade=" + classGrade +
+                ", name='" + name + '\'' +
+                ", enrolledSubjects=" + enrolledSubjects +
+                ", listOfEducation=" + listOfEducation +
+                ", listOfExperience=" + listOfExperience +
+                '}';
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public Grade getClassGrade() {
+        return classGrade;
+    }
+
+    public void setClassGrade(Grade classGrade) {
+        this.classGrade = classGrade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Subject> getEnrolledSubjects() {
+        return enrolledSubjects;
+    }
+
+    public void setEnrolledSubjects(ArrayList<Subject> enrolledSubjects) {
+        this.enrolledSubjects = enrolledSubjects;
+    }
+
+    public ArrayList<Education> getListOfEducation() {
+        return listOfEducation;
+    }
+
+    public void setListOfEducation(ArrayList<Education> listOfEducation) {
+        this.listOfEducation = listOfEducation;
+    }
+
+    public ArrayList<Experience> getListOfExperience() {
+        return listOfExperience;
+    }
+
+    public void setListOfExperience(ArrayList<Experience> listOfExperience) {
+        this.listOfExperience = listOfExperience;
+    }
 }
